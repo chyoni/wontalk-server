5a056fcbd43a992e892d2c47a2a9c1771b38a8b4
Chiwon <chiwon99881@gmail.com>
2019-08-03 14:32:10 +0900

    Refactoring 2019-08-03

--- src/api/Room/createRoom/createRoom.resolvers.ts
+++ src/api/Room/createRoom/createRoom.resolvers.ts
@@ -11,27 +11,61 @@ export default {
       withAuth(request);
       const { user } = request;
       const { you } = args;
-      try {
-        const room = await prisma.createRoom({
-          entrant: { connect: [{ id: user.id }] }
+      if (you.length === 1) {
+        const checkRoom = await prisma.$exists.room({
+          entrant_every: { OR: [{ id: user.id }, { id: you[0] }] }
         });
-        you.forEach(async youId => {
-          await prisma.updateRoom({
-            where: { id: room.id },
-            data: { entrant: { connect: { id: youId } } }
+        //엔트리에들어있는 모든사람들을 비교하는데 내가 지금만들려는 사람과의 방이
+        //이미존재하는지를 파악하는 거임 위에코드는ㅇㅇ
+        if (checkRoom) {
+          const existRoom = await prisma.rooms({
+            where: { entrant_every: { OR: [{ id: user.id }, { id: you[0] }] } }
           });
-        });
-        return {
-          ok: true,
-          error: null,
-          room: room as any
-        };
-      } catch (e) {
-        return {
-          ok: false,
-          error: e.message,
-          room: null
-        };
+          return {
+            ok: true,
+            error: null,
+            room: existRoom[0] as any
+          };
+        }
+        try {
+          const room = await prisma.createRoom({
+            entrant: { connect: [{ id: user.id }, { id: you[0] }] }
+          });
+          return {
+            ok: true,
+            error: null,
+            room: room as any
+          };
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+            room: null
+          };
+        }
+      } else {
+        try {
+          const room = await prisma.createRoom({
+            entrant: { connect: [{ id: user.id }] }
+          });
+          you.forEach(async youId => {
+            await prisma.updateRoom({
+              where: { id: room.id },
+              data: { entrant: { connect: { id: youId } } }
+            });
+          });
+          return {
+            ok: true,
+            error: null,
+            room: room as any
+          };
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+            room: null
+          };
+        }
       }
     }
   }
